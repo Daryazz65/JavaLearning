933b4846863c1c85e5b0c850ef179455cf1070b4
Дарья Зинина <zininadashuta167@gmail.com>
2026-02-19 02:05:21 +0300

    Изменен дизайн.

--- JavaLearning/Model/User.cs
+++ JavaLearning/Model/User.cs
@@ -27,6 +27,7 @@ namespace JavaLearning.Model
         public string Password { get; set; }
         public System.DateTime RegistrationDate { get; set; }
         public int IdRole { get; set; }
+        public byte[] Photo { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<QuestionUser> QuestionUsers { get; set; }
